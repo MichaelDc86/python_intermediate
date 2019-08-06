fe692a8f2eacb80875dbe51f2951cd5189c5108d
MichaelDc86 <lenskymiwa@ya.ru>
2019-08-06 14:17:39 +0300

    1

---
+++ lesson_x/lesson_1_pyqt.py
@@ -0,0 +1,51 @@
+import subprocess
+import ipaddress
+import random
+try:
+    from tabulate import tabulate
+except ModuleNotFoundError:
+    pass
+
+NUMBER_OF_IPS = 1
+IP_LENGTH = 4
+
+
+def get_yandex_different_ip():
+
+    address_list = []
+    for x in range(NUMBER_OF_IPS):
+
+        rand_address = '5.255.255.' + str(random.randint(0, 255))
+        address_list.append(rand_address)
+    ips_list = [ipaddress.ip_address(_).__str__() for _ in address_list]
+    return ips_list
+
+
+def get_address_list():
+    address_list = []
+    for x in range(NUMBER_OF_IPS):
+        rand_val_list = [str(random.randint(0, 63)*i) for i in range(1, IP_LENGTH + 1)]
+        rand_address = '.'.join(rand_val_list)
+        address_list.append(rand_address)
+    ips_list = [ipaddress.ip_address(_).__str__() for _ in address_list]
+    return ips_list
+
+
+def host_ping(ip_address_list):
+    result_list = []
+
+    for i in ip_address_list:
+        command = 'ping ' + i
+        result = bool(not subprocess.run(command, shell=True).returncode)
+        result_list.append(dict(host=i, result=result))
+    columns = ['reachable', 'unreachable']
+
+    # return tabulate(result_list, headers='keys', tablefmt='grid')
+    return tabulate(result_list_modified, headers=columns, tablefmt='grid')
+
+
+addrr_list = get_address_list()
+addrr_list.append('localhost')
+addrr_ya_list = get_yandex_different_ip()
+addrr_ya_list.append('localhost')
+print(host_ping(addrr_ya_list))
