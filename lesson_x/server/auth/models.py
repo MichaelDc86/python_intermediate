2c173523655d648092e160bd7c9aa915903bbe35
mike <lenskymiwa@ya.ru>
2019-09-27 10:41:35 +0300

    1

--- lesson_x/server/auth/models.py
+++ lesson_x/server/auth/models.py
@@ -3,6 +3,16 @@ from datetime import datetime
 from database import Base
 from sqlalchemy import Column, Integer, DateTime, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy_imageattach.entity import Image, image_attachment
+
+
+class UserPicture(Base, Image):
+    """User's profile picture."""
+
+    __tablename__ = 'user_picture'
+
+    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user = relationship('User')
 
 
 class User(Base):
@@ -12,6 +22,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
+    image = image_attachment('UserPicture')
     sessions = relationship('Session', back_populates='user')
     messages = relationship('Message', back_populates='user')
 
